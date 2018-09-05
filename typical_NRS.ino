d9976c34a6cb3f6ed2ee9eb9cf2df96739c7b909
Kim Young <42893834+typicaltype@users.noreply.github.com>
2018-09-05 22:58:52 +0900

    접점 추가
    42a, aux input 등 단자추가

--- typical_NRS.ino
+++ typical_NRS.ino
@@ -1,10 +1,15 @@
 /* 
 이 로직은 단방향 동작기기(START OR STOP)를 제어하기 위함이다.
-최종 수정일자 : 2018.09.04
+최종 수정일자 : 2018.09.05
 */ 
 
 const int input1 = 2; // START 스위치 입력
-const int input2 = 4; // STOP 스위치 입력
+const int input2 = 3; // STOP 스위치 입력
+const int input3 = 4; // 42A 입력
+const int input4 = 5; // NOT ASSIGN
+const int input5 = 6; // AUX Start 입력
+const int input6 = 7; // Aux Stop 입력
+const int input7 = 8; // 예비접점
 const int output1 = 8; // 제어신호 출력단자
 const int output2 = 9; // START feedback 신호
 const int output3 = 10; // STOP feedback 신호
@@ -20,6 +25,8 @@ boolean var6 = LOW; // 시리얼 ack 변수
 boolean comm_start = LOW; // START 최종신호
 boolean comm_stop = LOW; // STOP 최종신호
 boolean comm_ack = LOW; // ack 최종신호
+boolean aux_start = LOW; // Aux Start
+boolean aux_stop = LOW; // Aux Stop 
  
 boolean sr_q = LOW; // SR래치에 사용하는 변수
 boolean beep_feedback = LOW;
@@ -27,6 +34,11 @@ boolean beep_feedback = LOW;
 void setup() {
   pinMode(input1, INPUT); //START 스위치를 변수 input1로 설정
   pinMode(input2, INPUT); //STOP 스위치를 변수 input1로 설정
+  pinMode(input3, INPUT); //42A 접점을 변수 input3으로 설정
+  pinMode(input4, INPUT); //예비접점
+  pinMode(input5, INPUT); //aux start 명령 수신을input5로 설정
+  pinMode(input6, INPUT); //aux stop 명령 수신을input6으로 설정
+  pinMode(input7, INPUT); //예비접점
   pinMode(output1, OUTPUT); //출력을 변수 output1로 설정
   pinMode(output4, OUTPUT); //비프음 출력변수 output4로 설정
   Serial.begin(9600); // 시리얼통신 baudrate 설정
@@ -57,8 +69,7 @@ void loop() {
     Serial.write(comm); // putty창 표시용
     delay(100);
   }
-  //Serial 통신으로 data 읽기. 끝.
-    
+
   // 스위치입력과 통신입력 oring
   comm_start = var1 | var4;
   comm_stop = var2 | var5;
